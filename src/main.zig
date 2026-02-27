8f0314045b5dd4f71a938c76d84f9ccf0af6e766
Louis LeFebvre <louislefebvre1999@gmail.com>
2026-02-26 21:10:30 -0600

    chore: Cleanup build.zig

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
-const pgvector_playground = @import("pgvector_playground");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try pgvector_playground.bufferedPrint();
 }
